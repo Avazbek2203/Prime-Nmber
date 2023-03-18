c67bc90db66cfca257f6b5eca2cc08f58af64956
Avazbek2203 <muqumjonov2203@gmail.com>
2023-03-18 14:03:40 +0500

    Add project files.

---
+++ Ikkinchi darss/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        bool isNumber;
+        do
+        {
+            Console.Write(" Enter a number : ");
+            isNumber = int.TryParse(Console.ReadLine(), out number);
+            if (!isNumber)
+            {
+                Console.WriteLine("Please, input only number!");
+            }
+        } while (!isNumber);
+
+        if (number > 1)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(number + " is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a prime number.");
+            }
+        }
+        else
+        {
+            Console.WriteLine(number + "  is not a prime number.");
+        }
+    }
+}
+
+
+
